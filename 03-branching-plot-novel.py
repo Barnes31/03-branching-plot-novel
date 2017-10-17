98810e44eef5353ac1e3f0949466173ca3acf1cc
Kayla Barnes <barnes31@umail.iu.edu>
2017-10-17 01:55:19 -0400

    Branching plot novel
    Creates a branching plot novel game using python

---
+++ 03-branching-plot-novel.py
@@ -0,0 +1,53 @@
+import sys
+import json
+assert sys.version_info >= (3,4), 'This script requires at least Python 3.4'
+
+with open ('z_dict.py') as json_data:
+    world = json.load(json_data)
+def get_response (response):
+    try:
+        return int(response)-1
+    except ValueError:
+        return -1
+
+def print_response(count, responses):
+    return str(count +1)
+
+def check_quit (response):
+    response = str(response)
+    if response.lower() == 'q' or response.lower == 'quit':
+        return True
+    return False
+
+
+
+Running = True
+location = 'Begin'
+while Running:
+    #get the current location
+            current = world[location]
+            # print out the description of the current location
+            print(current['description'])
+            #print out the options
+            if current['options'] == []:
+                Running = False
+                continue
+            for count,option in enumerate(current['options']):
+                print('[' + print_response(count,current['options']) + '] ' + option['option'])
+            print('[q] to quit')
+            #get user response
+            response = input('\n\nWhat say you?'  )
+            #see if we need to quit
+            if check_quit(response):
+                Running = False
+                continue
+            #normalize response, convert to int
+            response = get_response(response)
+            #check response against options
+            for count,option in enumerate(current['options']):
+                if (response == count):
+                    location = option['goto']
+                    
+                 
+                
+print("\n\nHow did you do? Die too quickly? save the world? Go ahead...Try again! \n\n")
---
+++ z_dict.py
@@ -0,0 +1,172 @@
+{
+    "Begin" : {
+        "description":  "Long, long ago a plague of disease cursed the world, causing a slew of people to \n\nbecome the brain-eating, mindless drones that overrun the streets now; however, you, \n\nyou have made it thus far with your mind and body intact. You have been hiding out in \n\na bunker beneath your barn. Everyone had told you that you were crazy, but \n\nlook at them now-- roaming the streets as the undead. It has been years since you \n\nhave seen anyone...how much longer can you take it?\n\n",
+        "options": [
+            {
+                "option": "LOL forever: Stay inside until you die.",
+                "goto": "Ending 1"
+                },
+            {
+                "option": "I cannot do this anymore: Gather some supplies and leave immediately.",
+                "goto": "Option 2"
+                }
+            ]
+        },
+    "Option 2": {
+        "description": "You gather up a good amount of supplies and open the rusty hatch \n\nto your bunker. It is dark out.\n\n",
+        "options": [
+            {
+                "option": "Dark or light, there are always zombies out. Leave anyway.",
+                "goto": "Option 3"
+                },
+            {
+                "option": "Dark? No thanks. Wait until morning.",
+                "goto": "Option 4"
+                },
+            {
+                "option": "Forget it. I do not need to go out. Go back inside and stay there.",
+                "goto": "Ending 1"
+                }
+            ]
+        },
+    "Ending 1" : {
+        "description": "You eventually die old and alone. The End. \n\n",
+        "options": []
+    },
+"Option 3": {
+    "description": "You have been used to the dim light of your bunker, but it was no \n\ncomparison to the vast darkness of the empty spot of land you found \n\nyourself standing in. Off in the distance you hear a sea of low grumbling…\n\n",
+    "options": [
+        {
+            "option": "Zombies? Finally. Some action. Pull out your weapon and head toward the noise.",
+            "goto": "Ending 2"
+            },
+        {
+            "option" : "Too spooky 4 me. Go back inside and wait until morning. ",
+            "goto": "Option 4"
+            }
+        ]
+    },
+"Option 4": {
+    "description": "Congratulations, you were smart enough not to go out in the dark. You \n\nwake in the morning well rested, grab your (weapon), eat, and head out \n\ninto the bright clearing between your barn and house. Your house is \n\ncompletely covered in weeds and most of the windows are busted in.\n\n",
+    "options": [
+        {
+            "option" : "Ignore it. Head out in search of humans. ",
+            "goto": "Option 5"
+            },
+        {
+            "option": "Investigate. There might be humans inside.",
+            "goto": "Option 6"
+            }
+        ]
+    },
+ "Option 5": {
+     "description": "You head out past your barn quietly and follow the dirt road into town.\n\nEverything is quiet...too quiet.\n\n",
+     "options": [
+         {
+             "option" : "Search the stores for people.",
+             "goto": "Ending 4"
+             },
+         {
+             "option": "Keep walking.",
+             "goto": "Option 7"
+             }
+         ]
+     },
+ "Option 6": {
+     "description": "Sadly, there were no humans inside your house. Just a lot of painful \n\nmemories of what once was and rats...lots of rats. Unfortunately, your \n\nrustling attracts a nearby group of zombies and as you are leaving, you \n\nhear a low grumbling in the distance. What do you do?\n\n",
+     "options": [
+         {
+             "option": "Head towards the zombies. Might as well wipe a few out.",
+             "goto": "Option 8"
+             },
+         {
+             "option": "Run towards town.",
+             "goto": "Option 9"
+             },
+         {
+             "option": "Conserve your energy. Hide until they pass.",
+             "goto": "Ending 3"
+             }
+         ]
+     },
+ "Option 7": {
+     "description": "Sadly, you’d missed an opportunity to join a group of survivors and you \n\nwalk for days, sleeping in sewers and old buildings, until you stumble \n\nupon a large metal gate.\n\n",
+     "options": [
+         {
+             "option": "Knock.",
+             "goto": "Ending 5"
+             },
+         {
+             "option": "Keep walking.",
+             "goto": "Ending 6"
+             }
+         ]
+     },
+ "Option 8": {
+     "description": "Luckily, your daily workouts in the bunker had kept you strong and agile, \n\nand you found that the sun was not the zombies friend. They were \n\nnearly blind out in the daylight. You fight off several zombies, finding  \n\nthat the quickest, surefire way to quiet them is decapitation. Breathing \n\nheavily, covered in blood, you look around the empty clearing wondering \n\nwhat your next move should be.\n\n",
+     "options": [
+         {
+             "option": "There might be humans in town. Head towards town.",
+             "goto": "Option 9"
+             },
+         {
+             "option": "Zombies would be attracted to the city. Head away from town.",
+             "goto": "Ending 7"
+             }
+         ]
+     },
+ "Option 9": {
+     "description": "You wind up in town out of breath and tired with a few zombies following \n\nyou. Suddenly, you hear gunshots and the zombies fall to the ground. \n\nPeople...there were people here. And they had just saved you!\n\n",
+     "options": [
+         {
+             "option" : "Stay with them.",
+             "goto": "Ending 8"
+             },
+         {
+             "option": "Thank them and carry on. There could be other people out here.",
+             "goto": "Option 10"
+             }
+         ]
+     },
+ "Option 10": {
+     "description": "You walk for days, sleeping in sewers and old buildings, until you stumble \n\nupon a large metal gate. \n\n",
+     "options": [
+         {
+             "option": "Knock.",
+             "goto": "Ending 5"
+             },
+         {
+             "option": "Keep walking.",
+             "goto": "Ending 6"
+             }
+         ]
+     },
+ "Ending 2": {
+     "description": "Luckily your daily workouts in your bunker had kept you agile and strong; however, in \n\nthe heat of the fight, you neglect to notice the zombie coming up behind you and you \n\nget bitten. The change was rapid, your memories and thoughts fading quickly before \n\nyou, too, started to crave flesh.\n\n",
+     "options": []
+},
+ "Ending 3": {
+     "description": "Sadly, your cowardly plan to hide did not work out to your advantage. You had lost the \n\nelement of surprise, and the zombies had smelled your fear from several hundred \n\nfeet away. You were surrounded. No way out. \n\n",
+     "options": []
+ },
+ "Ending 4": {
+     "description": "By the 7th store, you turn around to find a short woman staring at you down the barrel \n\nof a gun. After a length examination, you are taken below ground where there is a \n\nsmall group of people who had also survived.You stay with them for years fighting \n\nzombies, and trying to rid the world of the disease. You end up marrying, and many,  \n\nmany years after you are dead and gone, someone ends up developing a cure for the \n\ndisease and saves the world.\n\n",
+     "options": []
+ },
+ "Ending 5": {
+     "description": "Your knock causes a slew of guns to be pointed at you. You looked awful, with torn \n\nclothes, from multiple zombie battles, and dried blood all over you. A man in a metal \n\nsuit comes to look at you checking to see if you had been bitten, and after careful \n\nconsideration he allows you into the compound. You are met with kindness and after \n\nseveral years, you even end up marrying and having children-- something you never \n\ndreamed possible. Many, many years after you are dead and gone, one of your great \n\ngreat grandchildren ends up developing a cure for the disease and saves the world.\n\n",
+     "options": []
+ },
+ "Ending 6": {
+     "description": "Your stupidity and cowardice leads to your demise. After days of walking in the hot sun, \n\nyou eventually run out of resources and starve. \n\n",
+     "options": []
+ },
+ "Ending 7": {
+     "description": "Your logic was, unfortunately, wrong. There were far more zombies away from town, and \n\nnot one person. You walked for days, your body tired from the continual exhaustion \n\nof fighting and walking. One hot day, you pass out from dehydration, and never \n\nwake up. Maybe you were picked off by zombies, maybe the fall was what killed \n\nyou, maybe it was the exhaustion and starvation. You will never know. \n\n",
+     "options": []
+     }
+ }
+
+
+
+            
